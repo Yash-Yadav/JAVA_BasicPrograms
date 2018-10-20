f0a56c9ec794d719d7f4d2c1141d6a6710bd4b71
Yash-Yadav <32845721+Yash-Yadav@users.noreply.github.com>
2018-10-20 20:38:25 +0530

    College Practicals

---
+++ College Practicals/MethodOverload.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+
+class MethodOverload
+{  
+    public static double area(double diameter)
+    {
+        return(3.14*diameter);
+    }
+
+     public static double area(double width, double height)
+    {
+        return(width*height);
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("Calculating area of Rectangle of dimension 25.5x35.5 m");
+        double area= area(25.5, 35.5);
+        System.out.println("Area of rectangle is " + area + " sq m");
+
+        System.out.println("Calculating area of Circle of diameter 2.5 m");
+        area = area(2.5);
+        System.out.println("Area of circle is " + area + " sq m");
+    }
+}
