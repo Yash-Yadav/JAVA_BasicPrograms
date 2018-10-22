3d838292a3373156ec37598deb9412d1d06b6fbd
Yash-Yadav <32845721+Yash-Yadav@users.noreply.github.com>
2018-10-22 09:19:01 +0530

    Update c2Inheritance.java

--- College Practicals/c2Inheritance.java
+++ College Practicals/c2Inheritance.java
@@ -5,16 +5,16 @@ class Parent
 {
   public Parent()
   {
-    System.out.println("0 Parameter Constructor Inide Parent");
+    System.out.println("0 Parameter Constructor Inside Parent");
   }
   public Parent(int a, int b)
   {
-    System.out.println("Constructor with 2 Integer type Parameters Inide Parent");
+    System.out.println("Constructor with 2 Integer type Parameters Inside Parent");
      System.out.println(a+b);
   }
   public Parent(float P, float T, float R)
   {
-    System.out.println("Constructor with 3 Float type Parameters Inide Parent");
+    System.out.println("Constructor with 3 Float type Parameters Inside Parent");
     System.out.print("Simple Interest of:\nP=100,T=1,R=10\n= ");
      System.out.println(P*T*R/100);
   }
@@ -25,7 +25,7 @@ class Child1 extends Parent
   public Child1()
   {
     super(100,10);
-    System.out.println("Inide Child1");
+    System.out.println("Inside Child1");
   }
 }
 
@@ -34,7 +34,7 @@ class Child2 extends Parent
   public Child2()
   {
     super(100,1,10);
-    System.out.println("Inide Child2");
+    System.out.println("Inside Child2");
 
   }
 }
