0132985072601f31e56928afde979c8e9b8ff034
Yash-Yadav <32845721+Yash-Yadav@users.noreply.github.com>
2018-11-01 19:37:52 +0530

    .JAVA Code

---
+++ College Practicals/JDBC.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBC
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver"); //Load Driver
+			Connection con = DriverManager.getConnection("jdbc:odbc:HOD_DATA"); //Create Connection with Data Source Name : HOD_DATA
+			Statement s = con.createStatement(); // Create Statement
+			String query = "select * from Data"; // Create Query
+			s.execute(query); // Execute Query 
+			ResultSet rs = s.getResultSet(); //return the data from Statement into ResultSet
+			while(rs.next()) // Retrieve data from ResultSet
+			{
+				System.out.print("Serial number : "+rs.getString(1)); //1st column of Table from database
+				System.out.print(" , Name : "+rs.getString(2)); //2nd column of Table 
+				System.out.print(" , City : "+rs.getString(3)); //3rd column of Table 
+				System.out.println(" and Age : "+rs.getString(4)); //4th column of Table 
+			}
+			s.close();
+			con.close();
+		}
+		catch (Exception e) 
+        {
+			System.out.println("Exception : "+e);
+		}
+	}
+}
